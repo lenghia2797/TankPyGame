843a100eba8db9500bcb352da1e514821da29d74
Nguyễn Trọng Phúc <ngtrongphuc1905@gmail.com>
2022-10-11 13:28:07 +0700

    fix score

--- ScoreLabel.py
+++ ScoreLabel.py
@@ -32,7 +32,7 @@ class ScoreLabel2:
         self.scene = scene
         self.font = pygame.font.Font('freesansbold.ttf', 30)
         self.text = self.font.render(
-            f'Score: {ScoreLabel.score}', True, Constants.COLOR_TEXT)
+            f'Score: {ScoreLabel2.score}', True, Constants.COLOR_TEXT)
         self.textRect = self.text.get_rect()
         self.textRect.center = (Constants.SCORE_LABEL_X,
                                 Constants.SCORE_LABEL_Y + 70)
@@ -42,7 +42,7 @@ class ScoreLabel2:
 
     def render(self):
         self.text = self.font.render(
-            f'Score: {ScoreLabel.score}', True, Constants.COLOR_TEXT)
+            f'Score: {ScoreLabel2.score}', True, Constants.COLOR_TEXT)
 
         self.scene.screen.blit(self.text, (self.textRect.x +
                                            self.textRect.width/2 - 50, self.textRect.y))
